0a32a17d2f87b1cf87dee0357d713e1dc0abcc89
zlt999 <ltzhang999@163.com>
2020-06-20 14:17:17 +0800

    Money increasing
    Realized clicking one button to increase money, which is display in top UI

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static MonoBehaviour instance { get; private set; }
+
+    private int currentMoney;
+
+    void Awake()
+    {
+        instance = this;
+        //DontDestroyOnLoad(gameObject);
+
+        currentMoney = 0;
+    }
+
+    public void AddMoney(int amount)
+    {
+        currentMoney += amount;
+        UIManager.instance.UpdateMoneyDisplay(currentMoney);
+    }
+    public int GetMoney() { return currentMoney; }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance { get; private set; }
+
+    public Text moneyText;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void UpdateMoneyDisplay(int currentMoney)
+    {
+        moneyText.text = currentMoney.ToString();
+    }
+}
